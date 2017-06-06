aa28f247c1fb2aba53aee5d279152fdc03fccec8
Patrick Spencer <pkspenc@gmail.com>
2017-06-06 11:19:15 -0400

    Delete file

--- makedb.py
+++
@@ -1,6 +0,0 @@
-import models
-from models import engine
-
-# This next line creates the table if it doesn't exists already.
-# The database specified in setting.py must already exist before you run this.
-models.EmployeeInfo.metadata.create_all(engine)
