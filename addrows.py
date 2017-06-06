dfd4b8933c7f1d4909c01205a88c00b06744c387
Patrick Spencer <pkspenc@gmail.com>
2017-06-06 09:21:06 -0400

    Update addrow

--- addrows.py
+++ addrows.py
@@ -5,12 +5,13 @@ year = '2004'
 month = '05'
 day = '03'
 date = "{} {} {}".format(year, month, day)
+
 employee_1 = EmployeeInfo(
         employee_id = 1,
         employee_name = 'Patrick',
-        year = '2004',
-        month = '05',
-        day = '03', 
+        year = year,
+        month = month,
+        day = date, 
         date = datetime.strptime(date, '%Y %m %d'),
         agency = 'F'
 )
